7182109f77c20c161bf73840e7f41558d7c2609d
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-08-31 15:02:35 -0300

    fix: finalizado e alterado para vercel update

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,7 +5,7 @@ import CardPerson from "../Developer";
 const Main = () => {
   return (
     <main className="Main">
-      <CardPerson name="Bruno" age="24 anos" country="Brasil" />
+      <CardPerson name="Bruno" country="Brasil" age="24 anos" />
       <CardPerson name="Igor" age="27 anos" country="Brasil" />
       <CardPerson name="Rafael" age="28 anos" country="Brasil" />
     </main>
